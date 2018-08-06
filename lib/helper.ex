3f185cadf5a2c82c62d83838412a79b74a9b52b6
Joakim Nylén <me@jnylen.nu>
2018-08-06 03:57:29 +0200

    Name helpers

--- lib/helper.ex
+++ lib/helper.ex
@@ -1,9 +1,17 @@
 defmodule Crawler.Helper do
     # Pretty up the name
     def name(name) do
-        # Different types of saying pack
-        pack_amounts = case Regex.named_captures(~r/(?<amount>[\d]+)( pack| tageslinsen| pk|pk|-pack|\/box| linser|er-box|er|stk| stk| stück|pcs| pcs)(s|)/i, name) do
+        add_pack_amounts = case Regex.named_captures(~r/\+ (?<amount>[\d]+)( pack| tageslinsen| pk|pk|-pack|\/box| linser|er-box|er|stk| stk| stück|pcs| pcs| lenses|pc| pc)(s|)/i, name) do
             %{"amount" => amount} -> String.to_integer(amount)
+            _ -> nil
+        end
+
+        # Different types of saying pack
+        pack_amounts = case Regex.named_captures(~r/(?<amount>[\d]+)( pack| tageslinsen| pk|pk|-pack|\/box| linser|er-box|er|stk| stk| stück|pcs| pcs| lenses|pc| pc)(s|)/i, name) do
+            %{"amount" => amount} -> case add_pack_amounts do
+                nil -> String.to_integer(amount)
+                add -> add + String.to_integer(amount)
+                end
             _ -> nil
         end
 
@@ -33,7 +41,7 @@ defmodule Crawler.Helper do
     def name_only(name) do
         # Clean up name
         name = Regex.replace(~r/(\d+)( |)x( |)(\d+) (Stück|stk\.)/i, name, "")
-        name = Regex.replace(~r/([\d]+)( pack| tageslinsen| pk|-box|box|-pack|-boxes| boxes|pk|\/box| linser|er pack|er-box|er|stk| stk| stück|pcs| pcs)(s|)/i, name, "")
+        name = Regex.replace(~r/([\d]+)( pack| tageslinsen| pk|-box|box|-pack|-boxes| boxes|pk|\/box| linser|er pack|er-box|er|stk| stk| stück|pcs| pcs| lenses|pc| pc)(s|)/i, name, "")
         name = Regex.replace(~r/contact lens(es|)/i, name, "")
         name = Regex.replace(~r/(contacts|Linser|lins|Piilolinssit)$/i, name, "")
         name = Regex.replace(~r/\[(Daily|Weekly|Monthly) contacts\]/i, name, "")
@@ -41,6 +49,7 @@ defmodule Crawler.Helper do
         name = Regex.replace(~r/(Daily|Weekly|Monthly|1-2 Week) (contacts)( Acuvue|)/i, name, "")
         name = Regex.replace(~r/(CIBA Vision|CooperVision|Cooper Vision|Bausch & Lomb)/i, name, "")
         name = Regex.replace(~r/Sparpaket( für|) (\d+) (Monate|M)/i, name, "")
+        name = Regex.replace(~r/\-(\d+)%/i, name, "")
         name = String.replace(name, ~r/ +/, " ")
         name = String.trim(name)
         name = String.replace(name, ~r/(Kontaktlinsen von \/ Alcon|Kontaktlinsen von Johnson & Johnson)/i, "")
@@ -53,8 +62,11 @@ defmodule Crawler.Helper do
         name = String.replace(name, ~r/ Box$/i, "")
         name = String.replace(name, ~r/ -$/, "")
         name = String.replace(name, ~r/,$/, "")
+        name = String.replace(name, ~r/\!$/, "")
         name = String.replace(name, ~r/(Daily|Weekly|Monthly|Montly|1-2 Week)$/i, "")
-        
+        name = String.replace(name, ~r/\+ free/i, "")
+        name = String.replace(name, ~r/new/i, "")
+        name = String.replace(name, ~r/starter set/i, "")
         
         # Clean up more
         name = String.replace(name, ~r/\'/, "")
